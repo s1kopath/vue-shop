b539bb6d6d90d79a704efa26ad12486592b5b065
Your name here <your_email@example.com>
2022-11-22 02:06:58 +0600

    bootstrap added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import './assets/app.scss'
+import jQuery from 'jquery'
+
+window.$ = window.jQuery = jQuery
+
+import 'popper.js'
 
 createApp(App).mount('#app')
